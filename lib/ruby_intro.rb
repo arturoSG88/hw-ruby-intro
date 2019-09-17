397789dc804cbb1e69152975d069d245617aed5f
arturoSG88 <arturosg88@gmail.com>
2019-09-17 15:33:24 -0500

    First commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,45 @@
 
 def sum arr
   # YOUR CODE HERE
+  if arr == []
+  	return 0
+  else
+  	s = 0
+  	arr.each {|x| s = (s + x)}
+  		return s
+  end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr == []
+  	return 0
+  elsif arr.length == 1
+  	return arr[0]
+  elsif arr.length == 2
+  	s = 0
+  	arr.each {|x| s = (s + x)}
+  	return s
+  else
+  	arr.sort! { |x,y| y <=> x }
+    s = arr[0] + arr[1]
+    return s
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length <= 1
+  	return false
+  else
+  	arr.each do |x|
+  		arr.each do |y|
+  			next if x == y
+  			return true if x + y == n
+  		end
+  	end
+  	return false
+  end
 end
 
 # Part 2
