fdaa6cd281088249dda7153bb6f2acf127524d1c
arturoSG88 <arturosg88@gmail.com>
2019-09-17 21:09:07 -0500

    Final commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -63,10 +63,41 @@ end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+	return true
+  else
+	return false
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    raise ArgumentError if @isbn == "" || @price < 1
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(str)
+    @isbn = str
+  end
+  
+  def price=(f)
+    @price = f
+  end
+  
+  def price_as_string
+    price_string = sprintf('%.2f', @price)
+    return "$%s" % price_string
+  end
 end
